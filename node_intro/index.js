6f04dfde27d24ebdbfde071c65cb0ee04f037747
immanuelpotter <manny.potter@capgemini.com>
2018-03-29 16:24:59 +0100

    Fixed tests by taking out listen in module and adding to the test

--- node_intro/index.js
+++ node_intro/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 var router = require('./routes/hello.js');
 
-var port = 3000;
-app.listen(port,function(){
-  console.log('Listening on port ' + port + '...');
-});
 app.use('/',router);
 
 module.exports = app;
