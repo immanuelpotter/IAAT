f669628909806e68ea7e4711e6ea40f851b30e5b
immanuelpotter <mannypotter@hotmail.com>
2018-05-05 18:31:56 +0100

    Added first 3 tasks of unit2-2

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # IAAT
 Internet Applications &amp; Techniques work as per practical notes
 
-node_intro - practical task 1 & 2 - basic express.js node server + API
+node_intro - Unit 1 - basic express.js node server + API
 	Directions for use: after cloning, make a "mongodata" directory
 	Make sure mongodb is installed
 	use mongod --dbpath mongodata/ before running tests
+
+php - Unit 2
+      make sure xampp is installed
+      run /opt/lampp/start.sh (as root)
+      add contents of this repo to /opt/lampp/htdocs (or whatever localhost is)
+      browse to localhost:80
+
+      for unit2-2 make sure DB is set up in phpmyadmin as specified in lab      
---
+++ unit2-2/calculator.php
@@ -0,0 +1,31 @@
+<?php
+
+if (isset($_GET["int1"]) && isset($_GET["int2"])){
+  $int1 = $_GET["int1"];
+  $int2 = $_GET["int2"];
+  echo "<p>Your first input is: " . $int1 . "</p>";
+  echo "<p>Your second input is: " . $int2 . "</p><br>";
+  
+  $add_res = $int1 + $int2 ;
+  print "<p>Addition result: " . $add_res . "</p>" ; 
+  
+  $minus_res = $int1 - $int2 ;
+  print "<p>Deduction result: " . $minus_res . "</p>";
+  
+  $mult_res = $int1 * $int2 ;
+  print "<p>Multiplication result: " . $mult_res . "</p>";
+ 
+  //can divide 0 by any int, however, cannot divide by 0  
+  if($int2 != 0){
+    $div_res = $int1 / $int2 ;
+    print "<p>Result: " . $div_res . "</p>" ; 
+  }
+  else {
+    print "Oh noez, divide by zeroez";
+  }
+}
+else {
+  echo "No unset inputs are allowed."; 
+}
+
+?>
